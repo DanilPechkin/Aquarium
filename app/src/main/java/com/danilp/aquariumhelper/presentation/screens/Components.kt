b589472b597cef3a6e9763b497971c63503eec5e
Danil <pppike15@gmail.com>
2022-07-24 15:54:04 +0500

    FromToInfoFields added

--- app/src/main/java/com/danilp/aquariumhelper/presentation/screens/Components.kt
+++ app/src/main/java/com/danilp/aquariumhelper/presentation/screens/Components.kt
@@ -18,8 +18,11 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.danilp.aquariumhelper.R
+import com.danilp.aquariumhelper.presentation.ui.theme.AquariumHelperTheme
 
 @Composable
 fun SearchField(
@@ -59,6 +62,38 @@ fun SearchField(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GridItem(
+    name: String,
+    message: String,
+    cardColors: CardColors,
+    //TODO: image
+    modifier: Modifier = Modifier
+) {
+    Card(
+        colors = cardColors,
+        shape = RoundedCornerShape(8.dp),
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(R.drawable.aquairum_pic),
+            contentDescription = name,
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier
+                .clip(RoundedCornerShape(8.dp))
+                .fillMaxWidth()
+        )
+        Column(
+            modifier = Modifier.padding(top = 6.dp, start = 10.dp, bottom = 10.dp, end = 10.dp)
+        ) {
+            Text(text = name, style = MaterialTheme.typography.titleMedium)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = message, style = MaterialTheme.typography.labelMedium)
+        }
+    }
+}
+
 @Composable
 fun InfoFieldWithError(
     value: String,
@@ -90,34 +125,64 @@ fun InfoFieldWithError(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GridItem(
-    name: String,
-    message: String,
-    cardColors: CardColors,
-    //TODO: image
-    modifier: Modifier = Modifier
+fun FromToInfoFields(
+    label: String,
+    valueFrom: String,
+    valueTo: String,
+    onValueFromChange: (String) -> Unit,
+    onValueToChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    keyboardOptions: KeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
+    errorMessageFrom: String? = null,
+    errorMessageTo: String? = null
 ) {
-    Card(
-        colors = cardColors,
-        shape = RoundedCornerShape(8.dp),
-        modifier = modifier
-    ) {
-        Image(
-            painter = painterResource(R.drawable.aquairum_pic),
-            contentDescription = name,
-            contentScale = ContentScale.FillWidth,
-            modifier = Modifier
-                .clip(RoundedCornerShape(8.dp))
-                .fillMaxWidth()
+    Column(modifier = modifier) {
+        Text(
+            text = label,
+            modifier = Modifier.padding(8.dp)
         )
-        Column(
-            modifier = Modifier.padding(top = 6.dp, start = 10.dp, bottom = 10.dp, end = 10.dp)
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Text(text = name, style = MaterialTheme.typography.titleMedium)
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(text = message, style = MaterialTheme.typography.labelMedium)
+            InfoFieldWithError(
+                value = valueFrom,
+                onValueChange = onValueFromChange,
+                label = stringResource(R.string.label_from),
+                modifier = Modifier
+                    .padding(end = 16.dp)
+                    .weight(1f),
+                keyboardOptions = keyboardOptions,
+                errorMessage = errorMessageFrom,
+                maxLines = 1,
+                singleLine = true
+            )
+            InfoFieldWithError(
+                value = valueTo,
+                onValueChange = onValueToChange,
+                label = stringResource(R.string.label_to),
+                modifier = Modifier.weight(1f),
+                keyboardOptions = keyboardOptions,
+                errorMessage = errorMessageTo,
+                maxLines = 1,
+                singleLine = true
+            )
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FromToInfoFieldsPreview() {
+    AquariumHelperTheme {
+        FromToInfoFields(
+            label = "Temperature",
+            valueFrom = "15",
+            valueTo = "20",
+            onValueFromChange = {},
+            onValueToChange = {}
+        )
     }
 }
