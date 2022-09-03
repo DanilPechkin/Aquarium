625ffac4461683c1becde339b6d5ff7f82158b3a
Danil <pppike15@gmail.com>
2022-09-03 12:17:29 +0500

    SettingsScreen.kt and AccountScreen.kt: added topAppBar

--- app/src/main/java/com/danilp/aquariumhelper/presentation/screens/top_bar_menu/account/AccountScreen.kt
+++ app/src/main/java/com/danilp/aquariumhelper/presentation/screens/top_bar_menu/account/AccountScreen.kt
@@ -1,15 +1,47 @@
 package com.danilp.aquariumhelper.presentation.screens.top_bar_menu.account
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.danilp.aquariumhelper.R
+import com.danilp.aquariumhelper.presentation.screens.AquariumTopBar
+import com.danilp.aquariumhelper.presentation.screens.destinations.AccountScreenDestination
+import com.danilp.aquariumhelper.presentation.screens.destinations.SettingsScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
+@OptIn(ExperimentalMaterial3Api::class)
 @RootNavGraph
 @Destination
 @Composable
 fun AccountScreen(
-
+    navigator: DestinationsNavigator
 ) {
-    Text(text = "Account")
+
+    var isTopMenuExpanded by rememberSaveable { mutableStateOf(false) }
+
+    Scaffold(
+        topBar = {
+            AquariumTopBar(
+                title = stringResource(R.string.account_title),
+                switchMenuVisibility = { isTopMenuExpanded = !isTopMenuExpanded },
+                isMenuExpanded = isTopMenuExpanded,
+                hideMenu = { isTopMenuExpanded = false },
+                navigateBack = { navigator.navigateUp() },
+                navigateToSettings = { navigator.navigate(SettingsScreenDestination) },
+                navigateToAccount = { navigator.navigate(AccountScreenDestination()) }
+            )
+        }
+    ) { paddingValues ->
+        Text(text = "account", modifier = Modifier.padding(paddingValues))
+    }
 }
--- app/src/main/java/com/danilp/aquariumhelper/presentation/screens/top_bar_menu/settings/SettingsScreen.kt
+++ app/src/main/java/com/danilp/aquariumhelper/presentation/screens/top_bar_menu/settings/SettingsScreen.kt
@@ -1,15 +1,46 @@
 package com.danilp.aquariumhelper.presentation.screens.top_bar_menu.settings
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.danilp.aquariumhelper.R
+import com.danilp.aquariumhelper.presentation.screens.AquariumTopBar
+import com.danilp.aquariumhelper.presentation.screens.destinations.AccountScreenDestination
+import com.danilp.aquariumhelper.presentation.screens.destinations.SettingsScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
+@OptIn(ExperimentalMaterial3Api::class)
 @RootNavGraph
 @Destination
 @Composable
 fun SettingsScreen(
-
+    navigator: DestinationsNavigator
 ) {
-    Text(text = "Settings")
+    var isTopMenuExpanded by rememberSaveable { mutableStateOf(false) }
+
+    Scaffold(
+        topBar = {
+            AquariumTopBar(
+                title = stringResource(R.string.settings_title),
+                switchMenuVisibility = { isTopMenuExpanded = !isTopMenuExpanded },
+                isMenuExpanded = isTopMenuExpanded,
+                hideMenu = { isTopMenuExpanded = false },
+                navigateBack = { navigator.navigateUp() },
+                navigateToSettings = { navigator.navigate(SettingsScreenDestination) },
+                navigateToAccount = { navigator.navigate(AccountScreenDestination()) }
+            )
+        }
+    ) { paddingValues ->
+        Text(text = "settings", modifier = Modifier.padding(paddingValues))
+    }
 }
